8434ca4ab97114712028164a372f46d87a39bcb4
Geraldine <gerahernan.al@gmail.com>
2023-02-09 16:38:48 -0800

    feat: added filter and select by year function

--- js/script.js
+++ js/script.js
@@ -4,17 +4,27 @@ const TRENDING_URL = BASE_URL + "/trending/all/day?api_key=" + APY_KEY;
 const SEARCH_URL = BASE_URL + '/search/movie?' + 'api_key=' + APY_KEY;
 const search = document.getElementById("search");
 
+const searchKeyword = document.getElementById('search-input');
+const selectYear = document.getElementById("select-year");
 // /genre/movie/list
 
 let movieList = [];
 
 let movieListCopy = [];
 
+const dataMovie = {
+    title: "",
+    name: "",
+    original_language: "",
+    overview: "",
+    poster_path: "",
+    release_date: ""
+}
+
 const generateUI = (array) => {
     let container = document.querySelector('.box-container');
     container.innerHTML = '';
-    console.log("que pasa aqui", array)
-    array.forEach(({ poster_path, title, original_language, overview }) => {
+    array.forEach(({ poster_path, title, original_language, overview, release_date }) => {
 
         let divContainer = document.createElement('div');
         let img = document.createElement('img');
@@ -22,17 +32,19 @@ const generateUI = (array) => {
         let description = document.createElement('p');
         let textContainer = document.createElement('div');
         let language = document.createElement('span');
+        let date = document.createElement('span');
 
         img.src = poster_path;
         name.textContent = `Name: ${title}`;
         description.textContent = `Overview : ${overview}`
         language.textContent = `Language: ${original_language} `
-
+        date.textContent = `Release date: ${release_date}`
         divContainer.appendChild(img);
 
         textContainer.appendChild(name);
         textContainer.appendChild(description);
         textContainer.appendChild(language);
+        textContainer.appendChild(date);
 
         divContainer.appendChild(textContainer);
 
@@ -43,13 +55,13 @@ const generateUI = (array) => {
 
 
 const parseList = (array) => {
-
-    return array.map(({ title, name, original_language, overview, poster_path }) => {
+    return array.map(({ title, name, original_language, overview, poster_path, release_date  }) => {
         return {
             title: title || name,
             overview: overview || 0,
             original_language: original_language,
-            poster_path: `https://image.tmdb.org/t/p/original/${poster_path}` || "https://via.placeholder.com/300"
+            poster_path: `https://image.tmdb.org/t/p/original/${poster_path}` || "https://via.placeholder.com/300",
+            release_date: release_date || 0
         }
     })
 
@@ -59,24 +71,69 @@ const getMoviesData = async (url) => {
     const response = await data.json();
     movieList = parseList(response.results);
     generateUI(movieList);
+    fillSelectByYear(movieList);
 }
 
-getMoviesData(TRENDING_URL);
 
-const searchMovie = () => {
-    let searchKeyword = document.getElementById('search-input');
-    let moviesFiltered = movieList.filter((movie) => {
-        let value = searchKeyword.value.toLowerCase();
-        console.log("VALUE", value)
 
-        if (movie.title.toLowerCase().indexOf(value) != -1) {
-            console.log("se cumple")
-            return true;
+document.addEventListener("DOMContentLoaded", () =>{
+    getMoviesData(TRENDING_URL);
+
+    fillSelectByYear()
+    
+})
+
+const fillSelectByYear = (movieList) =>{
+    let years = movieList.map(a => {
+        if(a.release_date != 0){
+            return a.release_date;
         }
-    })
-
-    movieList = moviesFiltered;
-    console.log("movieList", movieList)
-    generateUI(movieList);
+        return "2022-11-09";
+        
+    });
+    console.log(".release_date years:::", years)
+    for (let index = 0; index < years.length; index++) {
+        const option = document.createElement("option");
+        option.value = years[index];
+        option.textContent = years[index];
+        
+        selectYear.appendChild(option);
+    }
 }
 
+// Events:
+
+searchKeyword.addEventListener("keyup", e => {
+    dataMovie.title = e.target.value;
+    dataMovie.name = e.target.value;
+    FilterMovie();
+})
+
+selectYear.addEventListener("change", e => {
+    dataMovie.release_date = e.target.value;
+    FilterMovie();
+})
+
+const FilterMovie = () => {
+    const result = movieList.filter(filterByName).filter(filterByYear);
+    console.log("result",result)
+    return generateUI(result);
+}
+
+const filterByName = (movie) => {
+
+    const { title } = dataMovie;
+    if (title) {
+        return movie.title.toLowerCase().includes(title);
+    }
+    return movie;
+}
+
+const filterByYear = (movie) => {
+
+    const { release_date } = dataMovie;
+    if (release_date) {
+        return movie.release_date === release_date;
+    }
+    return movie;
+}
